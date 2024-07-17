185ea28f8ce40592100be72f67c785d8d5f8b8de
IforCU <tmdgus0844@gmail.com>
2024-07-17 20:19:15 +0900

    [11005] - 진법 문제2 수정

--- 11005.py
+++ 11005.py
@@ -2,9 +2,13 @@ digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 NFormation, BFormation = map(int,input().split())
 ans = ""
+if NFormation == 0:
+    ans = "0"
+else:
+    ans = ""
 
-while NFormation > 0:
-    ans += digits[NFormation % BFormation]
-    NFormation //= BFormation
+    while NFormation > 0:
+        ans = digits[NFormation % BFormation] + ans
+        NFormation //= BFormation
 
 print(ans)
