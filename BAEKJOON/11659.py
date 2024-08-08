f8a9d0ff15e4e633b7266ac7c5a63688d3af1706
IforCU <tmdgus0844@gmail.com>
2024-08-08 18:56:27 +0900

    [11659] - 문자열 나누기

--- BAEKJOON/11659.py
+++ BAEKJOON/11659.py
@@ -1,11 +1,23 @@
-N, M = map(int, input().split())
+import sys
+input = sys.stdin.read
 
-Number = list(map(int, input().split()))
+data = input().split()
 
-for i in range(M):
-    I, J = map(int, input().split())
-    if I == J:
-        print(Number[I-1])
-    else:
-        J = min(J, N)
-        print(sum(Number[I-1:J]))
+N = int(data[0])
+M = int(data[1])
+
+numbers = list(map(int, data[2:N+2]))
+
+prefix_sums = [0] * (N + 1)
+for i in range(1, N + 1):
+    prefix_sums[i] = prefix_sums[i - 1] + numbers[i - 1]
+
+query_start = N + 2
+results = []
+for _ in range(M):
+    i = int(data[query_start])
+    j = int(data[query_start + 1])
+    results.append(prefix_sums[j] - prefix_sums[i - 1])
+    query_start += 2
+
+sys.stdout.write('\n'.join(map(str, results)) + '\n')
