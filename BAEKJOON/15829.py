f503af199f23836ed52512e92c65eae7b4a67cb0
IforCU <tmdgus0844@gmail.com>
2024-09-30 00:05:33 +0900

    [15829] - hashing Large

--- BAEKJOON/15829.py
+++ BAEKJOON/15829.py
@@ -2,5 +2,5 @@ N = int(input())
 s = input()
 answer = 0
 for i in range(N):
-    answer += (int(ord(s[i]) - 96)) * (31**i)
-print(answer)
+    answer += (ord(s[i]) - 96) * (31**i)
+print(answer % 1234567891)
