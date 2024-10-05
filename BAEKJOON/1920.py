7e5251bc8e0b182e7b29c74644ef5eaadee6a8e4
IforCU <tmdgus0844@gmail.com>
2024-10-05 16:06:29 +0900

    [1920] - 수 찾기 in 사용

--- BAEKJOON/1920.py
+++ BAEKJOON/1920.py
@@ -1,26 +1,10 @@
 N = int(input())
-n = list(map(int, input().split()))
-n.sort()
+n = set(map(int, input().split()))
 M = int(input())
 m = list(map(int, input().split()))
 
-def list_find(list, x):
-    start = 0
-    end = len(list) - 1
-    while start <= end:
-        mid = (start + end) // 2
-        if list[mid] == x:
-            return True
-        elif list[mid] > x:
-            end = mid -1
-        else:
-            start = mid + 1
-    return False
- 
-
 for i in m:
-    if list_find(n,i):
-        print('1')
+    if i in n:
+        print(1)
     else:
-        print('0')
-    
+        print(0)
