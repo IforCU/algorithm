ba14c2e1fd55823eec3070683ffa14cd8e6779b7
IforCU <tmdgus0844@gmail.com>
2024-10-09 23:55:46 +0900

    [2579] - 계단 오르기

---
+++ BAEKJOON/2579.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+N = int(input().rstrip())
+stairs = [0] * (N+1)
+dp = [0] * (N+1)
+for i in range(1,N+1):
+    stairs[i] = int(input().rstrip())
+
+if N==1:
+    print(stairs[1])
+elif N==2:
+    print(sum(stairs))
+else:
+    dp[1] = stairs[1]
+    dp[2] = stairs[1] + stairs[2]
+    for i in range(3,N+1):
+        dp[i] = max(dp[i-2]+stairs[i], dp[i-3]+stairs[i-1]+stairs[i])
+    print(dp[-1])
