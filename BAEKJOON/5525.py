a2fd4a0d9d59e942d4e9879b9d0e8e02952f1de6
IforCU <tmdgus0844@gmail.com>
2024-10-17 15:40:35 +0900

    [5525] - IOIOI

--- BAEKJOON/5525.py
+++ BAEKJOON/5525.py
@@ -1,12 +1,17 @@
 N = int(input())
 M = int(input())
 S = input().rstrip()
-P = 'I'
-for _ in range(N):
-    P += 'OI'
-length = (2*N) + 1
-count = 0
-for i in range((M - (2*N))):
-    if S[i:i+length] == P:
-        count += 1
-print(count)
+ans, i, count = 0, 0, 0
+
+while i < (M-1):
+    if S[i:i+3] == 'IOI':
+        i += 2
+        count +=1
+        if count == N:
+            ans += 1
+            count -= 1
+    else:
+        i += 1
+        count = 0
+
+print(ans)
