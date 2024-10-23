e0d02ab9aeb0464725ee531e38cc491ebb257412
IforCU <tmdgus0844@gmail.com>
2024-10-23 14:55:10 +0900

    [15652] - N과 M (4)

---
+++ BAEKJOON/15652.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split())
+arr = [0 for _ in range(M)]
+visited = [False for _ in range(N)]
+
+def dfs(n, m, depth, start):
+    if depth == m:
+        print(' '.join(map(str, arr)))
+        return
+    
+    for i in range(start, n+1):
+        arr[depth] = i
+        dfs(n, m, depth + 1, i)
+
+dfs(N, M, 0, 1)
