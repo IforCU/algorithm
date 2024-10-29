76ae9455189267dbbf9e2db1fdf94086cb8d46bc
IforCU <tmdgus0844@gmail.com>
2024-10-29 15:45:37 +0900

    [1916] - 최소비용 구하기(우선순위큐 + dp + bfs)

--- BAEKJOON/1916.py
+++ BAEKJOON/1916.py
@@ -1,3 +1,4 @@
+import heapq
 import sys
 input = sys.stdin.readline
 N = int(input())
@@ -8,25 +9,22 @@ for i in range(M):
     city1, city2, cost = map(int, input().split())
     graph[city1].append((city2, cost))
 
-visited = [False for _ in range(N+1)]
+start, end = map(int, input().split())
+costs = [1e9 for _ in range(N+1)]
+heap = []
+costs[start] = 0
+heapq.heappush(heap, [0,start])
 
-ans = []
+while heap:
+    cost, v =  heapq.heappop(heap)
+    if costs[v] < cost:
+        continue
+    for dv, dCost in graph[v]:
+        sum_cost = cost + dCost
+        if costs[dv] <= sum_cost:
+            continue
+        
+        costs[dv] = sum_cost
+        heapq.heappush(heap, [sum_cost, dv])
 
-def dfs(x,y,cost):
-    if x == y:
-        ans.append((cost))
-        return
-    
-    for i, value in graph[x]:
-        if not visited[i]:
-            if ans:
-                if ans[-1] <= cost + value:
-                    continue
-            
-            visited[i] = True
-            dfs(i,y,value + cost)
-            visited[i] = False
-
-a, b = map(int, input().split())
-dfs(a,b,0)
-print(ans[-1])
+print(costs[end])
