db81441e623445e7c35b828a595c273dddef3da3
IforCU <tmdgus0844@gmail.com>
2024-10-30 15:04:43 +0900

    [9251] - LCS

---
+++ BAEKJOON/9251.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+A = ' '+input().rstrip()
+B = ' '+input().rstrip()
+dp = [[0 for _ in range(len(B))] for _ in range(len(A))]
+
+for i in range(1,len(A)):
+    for j in range(1,len(B)):
+        if A[i] == B[j]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+print(dp[-1][-1])
