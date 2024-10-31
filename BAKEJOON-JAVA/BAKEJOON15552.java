cfcc4fb566aa8e777750699145ed9cc7ffd6bfc2
IforCU <tmdgus0844@gmail.com>
2024-10-31 18:07:34 +0900

    [15552] - Java soultion 빠른 A+B

---
+++ BAKEJOON-JAVA/BAKEJOON15552.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+
+public class BAKEJOON15552 {
+    // 문제 풀이 예시로 만들고 다음과 같이 작성 후 복붙을 통해 Java 코드로 바꿈.
+    public void solution() throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<T; i++){
+            st = new StringTokenizer(br.readLine());
+            bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())) + "\n");
+        }
+
+        bw.close();
+    }
+}
