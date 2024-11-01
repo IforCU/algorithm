0ca79421a1fe7d7fbc9bf20011ff2f2f2ba53b9c
IforCU <tmdgus0844@gmail.com>
2024-11-01 10:05:43 +0900

    [10699] - Java 오늘 날짜

---
+++ BAKEJOON-JAVA/BAKEJOON10699.java
@@ -0,0 +1,17 @@
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.time.LocalDate;
+
+public class BAKEJOON10699 {
+    public void solution() throws Exception{
+        BufferedWriter br = new BufferedWriter(new OutputStreamWriter(System.out));
+        Date today = new Date();
+        SimpleDateFormat df = new SimpleDateFormat("YYYY-MM-dd");
+        String result = df.format(today);
+        br.write(LocalDate.now().toString() + "\n");
+        br.write(result);
+        br.close();
+    }
+}
