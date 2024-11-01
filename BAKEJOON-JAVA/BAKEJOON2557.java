603ee9b504573cbf3c1d4b6b7e289b3bf0fff2a9
IforCU <tmdgus0844@gmail.com>
2024-11-01 09:51:36 +0900

    [2557] - Java Hello World!

---
+++ BAKEJOON-JAVA/BAKEJOON2557.java
@@ -0,0 +1,10 @@
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class BAKEJOON2557 {
+    public void solution() throws Exception{
+        BufferedWriter br = new BufferedWriter(new OutputStreamWriter(System.out));
+        br.write("Hello World!");
+        br.close();
+    }
+}
