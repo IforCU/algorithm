9d95fff672d19ad08f32a18277bd673b07534dae
IforCU <tmdgus0844@gmail.com>
2024-11-14 21:33:23 +0900

    [1992] - 쿼드 트리 좌표방식 풀이

--- BAEKJOON/1992.py
+++ BAEKJOON/1992.py
@@ -2,35 +2,29 @@ import sys
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 N = int(input())
-graph = [list(map(int,input().strip())) for _ in range(N)]
+graph = [list(map(int, input().strip())) for _ in range(N)]
 
-def check(n,map):
-    first = map[0][0]
-    for i in range(n):
-        for j in range(n):
+def check(map, x, y, size):
+    first = map[y][x]
+    for i in range(y, y + size):
+        for j in range(x, x + size):
             if map[i][j] != first:
                 return False
     return True
 
-def quad_Tree(n, map):
+def quad_Tree(map, x, y, size):
     
-    if check(n, map):
-        return str(map[0][0])
+    if check(map, x, y, size):
+        return str(map[y][x])
     
-    half = n//2
-    
-    top_left = [row[:half] for row in map[:half]]
-    top_right = [row[half:] for row in map[:half]]
-    bottom_left = [row[:half] for row in map[half:]]
-    bottom_right = [row[half:] for row in map[half:]]
-    
+    half = size // 2
     temp = '('
-    temp += quad_Tree(half, top_left)
-    temp += quad_Tree(half, top_right)
-    temp += quad_Tree(half, bottom_left)
-    temp += quad_Tree(half, bottom_right)
+    temp += quad_Tree(graph, x, y, half)
+    temp += quad_Tree(graph, x + half, y, half)
+    temp += quad_Tree(graph, x, y + half, half)
+    temp += quad_Tree(graph, x + half, y + half, half)
     temp += ')'
 
     return temp
 
-print(quad_Tree(N,graph))
+print(quad_Tree(graph, 0, 0, N))
