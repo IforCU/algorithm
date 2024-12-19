c70bb5877292ca8b1261e7a846e00ffa78a8ccc4
IforCU <tmdgus0844@gmail.com>
2024-12-19 21:35:26 +0900

    [10942] - 팰린드롬?

---
+++ BAEKJOON/10942.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+arr = list(map(int, input().split()))
+M = int(input())
+dp = [[0] * N for _ in range(N)]
+
+for i in range(N):
+    dp[i][i] = 1
+
+for i in range(N-1):
+    if arr[i] == arr[i+1]:
+        dp[i][i+1] = 1
+
+for i in range(3, N+1):
+    for s in range(N-i + 1):
+        e = s + i - 1
+        if arr[s] == arr[e] and dp[s+1][e-1] == 1:
+            dp[s][e] = 1
+
+for _ in range(M):
+    S, E = map(int, input().split())
+    print(dp[S-1][E-1])
