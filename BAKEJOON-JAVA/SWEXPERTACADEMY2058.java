ca47ccaf230cf3dea04f7e13bf25c85c962a5291
IforCU <tmdgus0844@gmail.com>
2025-07-04 15:48:19 +0900

    [SW-2058] - 자릿수 더하기

---
+++ BAKEJOON-JAVA/SWEXPERTACADEMY2058.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class SWEXPERTACADEMY2058 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String N = sc.nextLine();
+        int sum = 0;
+        for (int i = 0; i < N.length(); i++) {
+            char c = N.charAt(i);
+            sum += c - '0';
+        }
+        System.out.println(sum);
+    }
+}
