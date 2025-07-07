4d605888b842b87f22e20355f0c50355c7f07e64
IforCU <tmdgus0844@gmail.com>
2025-07-07 15:46:45 +0900

    현대모비스 소프트웨어 아카데미

---
+++ BAKEJOON-JAVA/BAKEJOON26091.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BAKEJOON26091{
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        int left = 0;
+        int right = N - 1;
+        int result = 0;
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+            if (sum < M) left++;
+            else {
+                result++;
+                left++;
+                right--;
+            }
+            
+        }
+        System.out.println(result);
+    }
+}
