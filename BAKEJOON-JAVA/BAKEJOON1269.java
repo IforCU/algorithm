5007e6e5a76cef9bc964aa7b5a884102c74b2fdf
IforCU <tmdgus0844@gmail.com>
2025-07-08 19:22:01 +0900

    [1269] - 대칭 차집합

---
+++ BAKEJOON-JAVA/BAKEJOON1269.java
@@ -0,0 +1,32 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class BAKEJOON1269 {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        Set<Integer> setA = new HashSet<>();
+        Set<Integer> setB = new HashSet<>();
+
+        for (int i = 0; i < A; i++) {
+            setA.add(sc.nextInt());
+        }
+
+        for (int i = 0; i < B; i++) {
+            setB.add(sc.nextInt());
+        }
+
+        Set<Integer> A_diff = new HashSet<>(setA);
+        A_diff.removeAll(setB);
+        Set<Integer> B_diff = new HashSet<>(setB);
+        B_diff.removeAll(setA); 
+
+        A_diff.addAll(B_diff); 
+        System.out.println(A_diff.size());
+    }
+}
