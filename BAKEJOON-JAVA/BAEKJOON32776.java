7c3dd58af84a290dd08d7a9d7e6732cebf7fd1cc
IforCU <tmdgus0844@gmail.com>
2025-07-09 21:15:21 +0900

    [32776] - 가희와 4시간의 벽 2

---
+++ BAKEJOON-JAVA/BAEKJOON32776.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BAEKJOON32776 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int S = sc.nextInt();
+        int N = sc.nextInt() + sc.nextInt() + sc.nextInt();
+
+        if (S <= N || S <= 240 ) {
+            System.out.println("high speed rail");
+        } else {
+            System.out.println("flight");
+        }
+    }
+}
