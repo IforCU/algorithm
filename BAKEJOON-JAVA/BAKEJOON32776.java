a6e9d8e726625e3cb204cbd4af6099791b61cf2c
IforCU <tmdgus0844@gmail.com>
2025-07-15 10:01:03 +0900

    [MODI] - 파일명 오타 수정

--- BAKEJOON-JAVA/BAKEJOON32776.java
+++ BAKEJOON-JAVA/BAKEJOON32776.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BAEKJOON32776 {
+public class BAKEJOON32776 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int S = sc.nextInt();
