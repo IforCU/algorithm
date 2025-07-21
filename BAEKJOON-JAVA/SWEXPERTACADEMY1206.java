e58bf0d1009ac1ef2927fe4500c95c1771c6be0f
IforCU <tmdgus0844@gmail.com>
2025-07-21 11:23:16 +0900

    [SW1206] - [S/W 문제해결 기본] 1일차 - View

---
+++ BAEKJOON-JAVA/SWEXPERTACADEMY1206.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class SWEXPERTACADEMY1206 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = 10;
+        
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int N = sc.nextInt();
+            int[] arr = new int[N];
+            int sum = 0;
+            for (int i = 0; i < N; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            for (int i = 2; i < N-2; i++) {
+                int max = 0;
+                for (int j = -2; j <= 2; j++) {
+                    if (j == 0) continue;
+                    max = Math.max(max, arr[i + j]);
+                }
+                if (arr[i] > max) {
+                    sum += arr[i] - max;
+                }
+            }
+
+            System.out.println("#" + test_case + " " + sum);
+        }
+    }
+}
